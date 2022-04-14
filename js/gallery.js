0e9ca652b6a0e698285232ca1c274b3d6e6cd2a5
Aleksei-Mikheichev-Veb <Rorheh90@yandex.ru>
2022-04-15 00:23:34 +0300

    first commit

---
+++ js/gallery.js
@@ -0,0 +1,180 @@
+// Галерея
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const closeIcon = document.querySelector('.modal__close');
+const activeImage = document.querySelector('.modal__img');
+const galleryImages = document.querySelectorAll('.gallery__image');
+const galleryButtonLeft = document.querySelector('.modal__left');
+const galleryButtonRight = document.querySelector('.modal__right');
+
+galleryImages.forEach(galleryImage => { galleryImage.addEventListener('click', displayImage) });
+
+function displayImage(e) {
+    console.log('click');
+    this.classList.add('_active');
+    console.log(e.target);
+    activeImage.src = e.target.src;
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+closeIcon.addEventListener('click', closeImage);
+overlay.addEventListener('click', closeImage);
+function closeImage() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+galleryButtonRight.addEventListener('click', nextImage);
+function nextImage() {
+    for (var i = 0; i < galleryImages.length; i++) {
+        if (activeImage.src === galleryImages[i].src) {
+            var nextImageVar = galleryImages[i + 1];
+        }
+    }
+    if (nextImageVar === undefined) {
+        activeImage.src = galleryImages[0].src;
+    } else {
+        activeImage.src = nextImageVar.src;
+    }
+}
+
+galleryButtonLeft.addEventListener('click', prevImage);
+function prevImage() {
+    for (var i = 0; i < galleryImages.length; i++) {
+        if (activeImage.src === galleryImages[i].src) {
+            var nextImageVar = galleryImages[i - 1];
+        }
+    }
+    if (nextImageVar === undefined) {
+        activeImage.src = galleryImages[galleryImages.length - 1].src;
+    } else {
+        activeImage.src = nextImageVar.src;
+    }
+}
+
+
+// Инициализируем Swiper
+let myImageSlider = new Swiper('.image-slider', {
+    // Стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    // Включение/отключение
+    // перетаскивания на ПК
+    simulateTouch: true,
+    // Чувствительность свайпа
+    touchRatio: 1,
+    // Угол срабатывания свайпа/перетаскивания
+    touchAngle: 45,
+    // Курсор перетаскивания
+    grabCursor: true,
+
+    // Переключение при клике на слайд
+    slideToClickedSlide: false,
+
+    // Навигация по хешу
+    hashNavigation: {
+        // Отслеживать состояние
+        watchState: true,
+    },
+    // Автовысота
+    autoHeight: false,
+
+    // Количество слайдов для показа
+    slidesPerView: 6,
+
+    // Отключение функционала
+    // если слайдов меньше чем нужно
+    watchOverflow: false,
+
+    // Отступ между слайдами
+    spaceBetween: 10,
+
+    // Количество пролистываемых слайдов
+    slidesPerGroup: 1,
+
+    // Активный слайд по центру
+    centeredSlides: false,
+
+    // Стартовый слайд.
+    initialSlide: 0,
+
+    // Мультирядность
+    slidesPerColumn: 1,
+
+    // Бесконечный слайдер
+    loop: true,
+
+    // Кол-во дублирующих слайдов
+    loopedSlides: 0,
+
+    // Свободный режим
+    freeMode: true,
+
+    // Скорость
+    speed: 800,
+
+    // Эффекты переключения слайдов.
+    // Листание
+    effect: 'slide',
+
+
+
+    // Брейк поинты (адаптив)
+    // Ширина экрана
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        430: {
+            slidesPerView: 2,
+        },
+        650: {
+            slidesPerView: 3,
+        },
+        1100: {
+            slidesPerView: 4,
+        },
+        1300: {
+            slidesPerView: 6,
+        }
+    },
+
+
+    // Отключить предзагрузка картинок
+    preloadImages: false,
+    // Lazy Loading
+    // (подгрузка картинок)
+    lazy: {
+        // Подгружать на старте
+        // переключения слайда
+        loadOnTransitionStart: false,
+        // Подгрузить предыдущую
+        // и следующую картинки
+        loadPrevNext: false,
+    },
+    // Слежка за видимыми слайдами
+    watchSlidesProgress: false,
+    // Добавление класса видимым слайдам
+    watchSlidesVisibility: true,
+
+
+    // Обновить свайпер
+    // при изменении элементов слайдера
+    observer: true,
+
+    // Обновить свайпер
+    // при изменении родительских
+    // элементов слайдера
+    observeParents: true,
+
+    // Обновить свайпер
+    // при изменении дочерних
+    // элементов слайда
+    observeSlideChildren: true,
+
+});
+
---
+++ sendmail.php
@@ -0,0 +1,75 @@
+<?php
+	 ini_set('display_errors', 1);
+	 ini_set('display_startup_errors', 1);
+	 error_reporting(E_ALL);
+
+	 use PHPMailer\PHPMailer\PHPMailer;
+	 use PHPMailer\PHPMailer\Exception;
+	 
+
+	 require 'phpmailer/src/Exception.php';
+	 require 'phpmailer/src/PHPMailer.php';
+	 require 'phpmailer/src/SMTP.php';
+
+	 $mail = new PHPMailer(true);
+	 $mail->CharSet = 'UTF-8';
+	 $mail->setLanguage('ru', 'phpmailer/language/');
+	 $mail->IsHTML(true);
+
+     $mail->Host = 'mail.hosting.reg.ru';
+	 $mail->Port = 465;
+	 $mail->Username = 'info@doktoronko.com';
+	 $mail->Password = '19840414aAa';
+	
+    //  $mail->Host = 'smtp.gmail.com';
+	//  $mail->Port = 465;
+	//  $mail->Username = 'aleksejmiheicev61834@gmail.com';
+	//  $mail->Password = 'mixei5063757';
+	//От кого письмо
+	$mail->setFrom('info@doktoronko.com');
+	//Кому отправить
+	$mail->addAddress('gluk-90@list.ru');
+	$mail->addAddress('info@doktoronko.com');
+	 //Тема письма
+     $mail->Subject = 'Письмо с моего сайта';
+
+
+	 //Тело письма
+	 $body = '<h1>Здравствуйте</h1>';
+	
+	//  Сюда передаются данные из скрытого поля
+     if(trim(!empty($_POST['which']))){
+        $body.='<h1><strong></strong> '.$_POST['which'].'</h1>';
+    }
+	 if(trim(!empty($_POST['name']))){
+	 	$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	 }
+	 if(trim(!empty($_POST['phone']))){
+	 	$body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+	 }
+	 if(trim(!empty($_POST['email']))){
+	 	$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	 }
+	
+	 if(trim(!empty($_POST['message']))){
+	 	$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	 }
+	
+	 $mail->msgHTML($body);
+
+	// //Отправляем
+	 try {
+	 	if (!$mail->send()) {
+	 		$message = 'Ошибка';
+	 	} else {
+	 		$message = 'Данные отправлены!';
+	 	}
+	 } catch (\Exception $e) {
+	 	$message = $e->getMessage();
+	 }
+
+	 $response = ['message' => $message];
+
+	 header('Content-type: application/json');
+	 echo json_encode($response);
+?>
